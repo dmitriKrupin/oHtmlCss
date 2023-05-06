2be577e5a97a00ddc7530b0867e174b0a736fdc8
Dmitriy Krupin <dmitri.krupin@yandex.ru>
2023-05-06 11:33:26 +0300

    Popup menu for login has been added.

--- SCRIPTS/script.js
+++ SCRIPTS/script.js
@@ -23,3 +23,25 @@ function outputUpdate(vol, name) {
         output.style.left = vol - 55 + 'px';
     }
 }
+
+const wrapperLogin = document.querySelector('.wrapper-login');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.header-content__log-in');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', () => {
+    wrapperLogin.classList.add('active'); //добавляем active к тэгу wrapper-login
+});
+
+loginLink.addEventListener('click', () => {
+    wrapperLogin.classList.remove('active'); //удаляем active к тэгу wrapper-login
+}); 
+
+btnPopup.addEventListener('click', () => {
+    wrapperLogin.classList.add('active-popup'); //добавляем active-popup к тэгу wrapper-login 
+});
+
+iconClose.addEventListener('click', () => {
+    wrapperLogin.classList.remove('active-popup'); //добавляем active-popup к тэгу wrapper-login 
+});
